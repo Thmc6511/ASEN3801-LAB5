4e686c9846c72db2b25838ead461cff78611ffb0
alexlu33 <allu3585@colorado.edu>
2025-11-18 15:26:59 -0700

    New problem 2

---
+++ Problem2.m
@@ -0,0 +1,13 @@
+clc
+clear
+close all
+
+%% Problem 2.1
+xdot = AircraftEOM(time, aircraft_state, aircraft_surfaces, wind_inertial,aircraft_parameters)
+
+% Define variables
+x0 = zeros(12,1); % aircraft state
+x0(3) = -1609.34;% altitude (m)
+x0(7) = 21.0;% u (m/s)
+u = [0; 0; 0; 0];% control surface deflections in rad
+wind = [0; 0; 0];% inertial wind (m/s)
