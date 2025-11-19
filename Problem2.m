541bee32092a542d1626b8d67367d3e2ba188774
alexlu33 <allu3585@colorado.edu>
2025-11-18 17:11:55 -0700

    Added 2.2 and 2..3

--- Problem2.m
+++ Problem2.m
@@ -2,18 +2,31 @@ clc
 clear
 close all
 
+ttwistor; % Load in aircraft parameters from ttwistor.m
+ap = aircraft_parameters;
+
+tspan = [0 200]; % time span for ode45
 %% Problem 2.1
+%{
 % Define variables
 x0 = zeros(12,1); % aircraft state
 x0(3) = -1609.34;% altitude (m)
 x0(7) = 21.0;% u (m/s)
 u = [0; 0; 0; 0];% control surface deflections in rad
 wind = [0; 0; 0];% inertial wind (m/s)
+%}
 
-ttwistor; % Load in aircraft parameters from ttwistor.m
-ap = aircraft_parameters;
+%% Problem 2.2
+%{
+x0 = [0; 0; -1800; 0; 0.02780; 0; 20.99; 0; 0.5837; 0; 0; 0];
+u = [0.1079; 0; 0; 0.3182];
+wind = [0; 0; 0];
+%}
 
-tspan = [0 200]; % time span for ode45
+%% Problem 2.3
+x0 = [0; 0; -1800; deg2rad(15); deg2rad(-12); deg2rad(270); 19; 3; -2; 0.08; -0.2; 0];
+u = [deg2rad(5); deg2rad(2); deg2rad(-13); 0.3];
+wind = [0; 0; 0];
 
 %% Ode call
 odefun = @(t,x) AircraftEOM(t, x, u, wind, ap);
